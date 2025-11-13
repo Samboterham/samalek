593c779e327887f23d27a38ea3c205fd3b02d65b
Samboterham <berghoefsam79@gmail.com>
2025-11-13 09:19:32 +0100

    images erin gezet

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" class="stylesheet">
+</head>
+
+<body>
+    <div class="grid">
+        <img src="Assets/images/UAimg1.png" alt="UAimg1">
+        <img src="Assets/images/UAimg2.png" alt="UAimg2">
+        <img src="Assets/images/UAimg3.png" alt="UAimg3">
+        <img src="Assets/images/UAimg4.png" alt="UAimg4">
+        <img src="Assets/images/UAimg5.png" alt="UAimg5">
+        <img src="Assets/images/UAimg6.png" alt="UAimg6">
+        <img src="Assets/images/UAimg7.png" alt="UAimg7">
+        <img src="Assets/images/UAimg8.png" alt="UAimg8">
+        <img src="Assets/images/UAimg9.png" alt="UAimg9">
+        <img src="Assets/images/UAimg10.png" alt="UAimg10">
+        <img src="Assets/images/UAimg11.png" alt="UAimg11">
+        <img src="Assets/images/UAimg12.png" alt="UAimg12">
+        <img src="Assets/images/UAimg13.png" alt="UAimg13">
+        <img src="Assets/images/UAimg14.png" alt="UAimg14">
+        <img src="Assets/images/UAimg15.png" alt="UAimg15">
+        <img src="Assets/images/UAimg16.png" alt="UAimg16">
+        <img src="Assets/images/UAimg17.png" alt="UAimg17">
+        <img src="Assets/images/UAimg18.png" alt="UAimg18">
+        <img src="Assets/images/UAimg19.png" alt="UAimg19">
+        <img src="Assets/images/UAimg20.png" alt="UAimg20">
+        <img src="Assets/images/UAimg21.png" alt="UAimg21">
+        <img src="Assets/images/UAimg22.png" alt="UAimg22">
+        <img src="Assets/images/UAimg23.png" alt="UAimg23">
+        <img src="Assets/images/UAimg24.png" alt="UAimg24">
+        <img src="Assets/images/UAimg25.png" alt="UAimg25">
+        <img src="Assets/images/UAimg26.png" alt="UAimg26">
+        <img src="Assets/images/UAimg27.png" alt="UAimg27">
+        <img src="Assets/images/UAimg28.png" alt="UAimg28">
+        <img src="Assets/images/UAimg29.png" alt="UAimg29">
+        <img src="Assets/images/UAimg30.png" alt="UAimg30">
+        <img src="Assets/images/UAimg31.png" alt="UAimg31">
+        <img src="Assets/images/UAimg32.png" alt="UAimg32">
+        <img src="Assets/images/UAimg33.png" alt="UAimg33">
+    </div>
+</body>
+
+</html>
